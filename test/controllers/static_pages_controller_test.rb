1fc2180811b6ea261f208de677b05fb8e6a78641
FrancisPatrick <francispatrickcanlas@gmail.com>
2016-03-05 01:09:18 +0800

    Add title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "MicroBlog"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "Help | MicroBlog"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "About | MicroBlog"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | MicroBlog"
   end 
 
 end
