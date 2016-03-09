b82dd079a48124404db8bd661e19d8457c614c37
FrancisPatrick <francispatrickcanlas@gmail.com>
2016-03-10 00:59:27 +0800

    Add remember token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,14 @@ module SessionsHelper
 		session[:user_id]=user.id
 	end
 
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
@@ -17,7 +24,7 @@ module SessionsHelper
 
 	def current_user
 		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: :user_id)
+			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
 			if user && user.authenticated?(cookies[:remember_token])
@@ -26,7 +33,6 @@ module SessionsHelper
 			end
 		end
 	end
-
 
 	def logged_in?
 		!current_user.nil?
